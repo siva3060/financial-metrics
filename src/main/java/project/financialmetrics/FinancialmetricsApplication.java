83658089ebdb8eed673b54120728da236dcb4419
siva <siva3060@gmail.com>
2020-01-02 23:51:18 +0530

    CREATED TEMPLATE @ FINANCIAL METICS
    created an template to the project added all the
    requited dependencies using the starter spring
    tool and linked to the github url name
    https://github.com/siva3060/financial-metrics.git

---
+++ src/main/java/project/financialmetrics/FinancialmetricsApplication.java
@@ -0,0 +1,13 @@
+package project.financialmetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialmetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialmetricsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/financialmetrics/FinancialmetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package project.financialmetrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialmetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
